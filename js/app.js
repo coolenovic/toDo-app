2ac7fef10730101064296dcb07ba5d6fcdd0f029
koola <coolenovic@hotmail.com>
2020-11-11 14:26:55 +0100

    select the Elements

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// CODE EXPLAINED channel
+
+//Select the Elements
+const clear = document.querySelector(".clear");
+const dataElement = document.getElementById("data");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
