415bff0fe8fd8866f26a8dd3799cb190333a74ff
koola <coolenovic@hotmail.com>
2020-11-11 16:39:56 +0100

    added class names and todays date to app

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@
 
 //Select the Elements
 const clear = document.querySelector(".clear");
-const dataElement = document.getElementById("data");
+const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
+
+
+// Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Show todays date
+const options = {weekday:"long", month:"short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
